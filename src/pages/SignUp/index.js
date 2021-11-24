a1538c3d8defac99c1b68c04e7da7e63fd5df96b
Iann-rst <iannrst11@gmail.com>
2021-11-24 09:56:17 -0300

    Página de cadastro (SignUp)

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,34 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
+
+//Página de cadastro
 function SignUp() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [nome, setNome] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault(); //para não atualizar a página
+    alert('Clicou');
+  }
+
   return (
-    <div>
-      <h1>Tela de cadastro</h1>
+    <div className="container-center">
+      <div className="login">
+        <div className="logo-area">
+          <img src={logo} alt="Logo do Sistema" />
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <h1>Cadastrar uma conta</h1>
+          <input type="text" placeholder="Digite seu nome" value={nome} onChange={(e) => setNome(e.target.value)} />
+          <input type="text" placeholder="Digite seu email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="password" placeholder="Digite sua senha" value={senha} onChange={(e) => setSenha(e.target.value)} />
+          <button type="submit">Acessar</button>
+        </form>
+        <Link to="/">Já tem uma conta?</Link>
+      </div>
     </div>
   );
 }
