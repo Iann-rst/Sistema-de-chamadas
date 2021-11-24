72ab723b73790e36c2f5db2662cfc171b955529b
Iann-rst <iannrst11@gmail.com>
2021-11-24 10:08:20 -0300

    Contexto de autenticação

---
+++ src/contexts/auth.js
@@ -0,0 +1,21 @@
+/*Contexto de autenticação */
+
+import { useState, createContext, useEffect } from 'react';
+import firebase from '../services/firebaseConnection';
+
+/*Criação do contexto */
+export const AuthContext = createContext();
+
+function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loadingAuth, setLoadingAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  return (
+    <AuthContext.Provider value={{ signed: !!user, user }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider;
