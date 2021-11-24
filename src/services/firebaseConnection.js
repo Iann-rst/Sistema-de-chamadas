5015de83e064c9e11f8816295a6eadd02a97b48c
Iann-rst <iannrst11@gmail.com>
2021-11-24 09:42:42 -0300

    Conexão com o firebase

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+/**
+ * Configuração e inicialização do firebase
+ */
+
+let firebaseConfig = {
+  apiKey: "AIzaSyB4GwmIKfPjNGPDHSAuNJixIn8_xHc14Rg",
+  authDomain: "sistema-a7e1c.firebaseapp.com",
+  projectId: "sistema-a7e1c",
+  storageBucket: "sistema-a7e1c.appspot.com",
+  messagingSenderId: "667587973043",
+  appId: "1:667587973043:web:614deba0d1f3a3867ec5c6",
+  measurementId: "G-D08V49DB4X"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
