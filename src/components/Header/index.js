d4b3c691897e82de544c4c9d6d61dd2290dc660b
Iann-rst <iannrst11@gmail.com>
2021-11-25 08:45:12 -0300

    Componente Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -22,16 +22,19 @@ export default function Header() {
         <img src={user.avatarUrl === null ? avatar : user.avatarUrl} alt="Foto Perfil" />
       </div>
 
+      {/* */}
       <Link to="/dashboard">
         <FiHome color="#FFF" size={24} />
         Chamados
       </Link>
 
-      <Link to="/clientes">
+      {/*Página Clientes*/}
+      <Link to="/customers">
         <FiUsers color="#FFF" size={24} />
         Clientes
       </Link>
 
+      { /*Página perfil */}
       <Link to="/profile">
         <FiSettings color="#FFF" size={24} />
         Configurações
